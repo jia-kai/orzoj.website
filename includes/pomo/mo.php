1c50146a9798f348e79a59e38f9309eefad2c5b8
fqj1994 <fqj1994@gmail.com>
2010-10-12 11:04:55 +0000

    Change License of pomo library to Apache License so that everyone can use it freely.

--- includes/pomo/mo.php
+++ includes/pomo/mo.php
@@ -1,28 +1,29 @@
 <?php
 /*
  * $File: mo.php
- * $Date: Wed Sep 29 14:25:41 2010 +0800
+ * $Date: Tue Oct 12 11:03:54 2010 UTC
+ * $Author: Qijiang Fan <fqj1994@gmail.com>
+ * This file is edited for Orz Online Judge
  */
 /**
  * @package orzoj-website
  * @subpackage l10n
- * @license http://gnu.org/licenses/ GNU GPLv3
+ * @license http://apache.org/licenses/LICENSE-2.0.txt Apache License
  */
 /*
-	This file is part of orzoj
+   Copyright [2010] [Qijiang Fan]
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+       http://www.apache.org/licenses/LICENSE-2.0
 
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  */
 
 if (!defined('IN_ORZOJ')) exit;
