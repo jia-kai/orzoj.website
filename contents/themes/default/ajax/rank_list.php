da13f70197bbc7fbdf420e9e766bee5e8a0f3dab
zxytim <zxytim@gmail.com>
2010-10-21 13:22:14 +0800

    rank-list improved

--- contents/themes/default/ajax/rank_list.php
+++ contents/themes/default/ajax/rank_list.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * $File: rank_list.php
- * $Date: Wed Oct 20 22:37:44 2010 +0800
+ * $Date: Thu Oct 21 13:21:46 2010 +0800
  */
 /**
  * @package orzoj-website
@@ -29,6 +29,13 @@ if (!defined('IN_ORZOJ'))
 $USERS_PER_PAGE = 50;
 $RANK_LIST_COLOR_SELF = '#7f7f7f';
 
+$sort_list = array(
+	array('cnt_ac_prob', 'DESC'),
+	array('ac_ratio', 'DESC'),
+	array('cnt_submit_prob', 'DESC')
+);
+
+
 $start_page = 1;
 if (isset($_POST['sort_col']) && isset($_POST['sort_way']))
 {
@@ -40,8 +47,8 @@ if (isset($_POST['sort_col']) && isset($_POST['sort_way']))
 }
 else
 {
-	$sort_col = 'cnt_ac_prob';
-	$sort_way = 'DESC';
+	$sort_col = $sort_list[0][0];
+	$sort_way = $sort_list[0][1];
 }
 
 if (isset($page_arg))
@@ -50,8 +57,15 @@ if (isset($page_arg))
 		die(__('Unknown argument.'));
 }
 
+$user_amount = $db->get_number_of_rows('users');
+$total_page = ceil($user_amount / $USERS_PER_PAGE);
+
+if ($start_page < 1) $start_page = 1;
+if ($start_page > $total_page) $start_page = $total_page;
+
 $start_rank = ($start_page - 1) * $USERS_PER_PAGE + 1;
 $rank_delta = 1;
+$default_sort_col = $sort_list[0][0];
 ?>
 
 <script type="text/javascript">
@@ -64,8 +78,6 @@ echo <<<EOF
 <script type="text/javascript">
 function table_sort_by(col, default_order)
 {
-	if (col == "rank")
-		col = sort_col;
 	if (sort_col == col)
 	{
 		if (sort_way == "ASC")
@@ -107,13 +119,6 @@ $heads = array(
 	array(__('AC ratio'), 'ac_ratio', 'DESC')
 );
 
-$sort_list = array(
-	array('cnt_ac_prob', 'DESC'),
-	array('ac_ratio', 'DESC'),
-	array('cnt_submit_prob', 'DESC'),
-	array('nickname', 'DESC')
-);
-
 ?>
 </script>
 
@@ -130,7 +135,24 @@ foreach ($heads as $head)
 	_make_table_header($head[0], $head[1], $head[2]);
 echo '</tr>';
 
+/**
+ * @ignore
+ */
+function op_order($order) 
+{
+	return $order == 'ASC' ? 'DESC' : 'ASC';
+}
+
+
 $orderby = array();
+if ($sort_col == 'rank')
+{
+	$sort_col = $sort_list[0][0];
+	if ($sort_way == 'ASC')
+		$sort_way = $sort_list[0][1];
+	else
+		$sort_way = op_order($sort_list[0][1]);
+}
 $orderby[$sort_col] = $sort_way;
 $is_default_order = FALSE;
 foreach ($sort_list as $val)
@@ -142,14 +164,6 @@ foreach ($sort_list as $val)
 			$is_default_order = FALSE;
 		break;
 	}
-/**
- * @ignore
- */
-function op_order($order) 
-{
-	return $order == 'ASC' ? 'DESC' : 'ASC';
-}
-
 foreach ($sort_list as $val)
 	if ($val[0] != $sort_col)
 		$orderby[$val[0]] = ($is_default_order ? $val[1] : op_order($val[1]));
@@ -162,26 +176,76 @@ $users = $db->select_from('users',
 	$USERS_PER_PAGE
 );
 
-if (!$is_default_order)
+
+$get_rank_vesion = 2;
+if ($get_rank_vesion == 1)
 {
-	$start_rank = user_get_user_amount() - $start_rank + 1;
-	$rank_delta = -1;
+	/* get rank version 1 */
+	/**
+	 * @ignore
+	 */
+	function _get_rank($uid)
+	{
+		global $db, $DBOP, $sort_list;
+		$tmp = NULL;
+		$_get_rank_where = NULL;
+		foreach ($sort_list as $sort)
+		{
+			$t = $tmp;
+			$val = $db->select_from('users', array($sort[0]), array($DBOP['='], 'id', $uid));
+			$val = $val[0][$sort[0]];
+			db_where_add_and($t, array($DBOP[$sort[1] == 'ASC' ? '<' : '>'], $sort[0], $val));
+			db_where_add_or($_get_rank_where, $t);
+			db_where_add_and($tmp, array($DBOP['='], $sort[0], $val));
+		}
+		return $db->get_number_of_rows('users', $_get_rank_where) + 1;
+	}
 }
-/**
- * @ignore
- */
-function _get_rank()
+else if ($get_rank_vesion == 2)
 {
-	global $start_rank, $rank_delta;
-	$ret = $start_rank;
-	$start_rank += $rank_delta;
-	return $ret;
+	/* get rank version 2 */
+	$order_by = array();
+	$cols = array('id');
+	foreach ($sort_list as $sort)
+	{
+		$order_by[$sort[0]] = $sort[1];
+		$cols[] = $sort[0];
+	}
+	$tmp = $db->select_from('users', $cols, NULL, $order_by);
+	$id2rank = array();
+	$cnt = 0;
+	$now = 1;
+	function cmp($user1, $user2)
+	{
+		global $sort_list;
+		foreach($sort_list as $sort)
+			if ($user1[$sort[0]] != $user2[$sort[0]])
+			{
+				return $sort[1] == 'ASC' ? ($user1[$sort[0]] < $user2[$sort[0]]) : ($user1[$sort[0]] > $user2[$sort[0]]);
+			}
+		return false;
+	}
+	$prev_user = NULL;
+	foreach ($tmp as $_user)
+	{
+		if ($cnt > 0 && !cmp($prev_user, $_user))
+			$id2rank[$_user['id']] = $now;
+		else
+			$id2rank[$_user['id']] = $now = $cnt + 1;
+		$cnt ++;
+		$prev_user = $_user;
+	}
+	function _get_rank($uid)
+	{
+		global $id2rank;
+		return $id2rank[$uid];
+	}
 }
 
 foreach ($users as $_user)
 {
 	echo '<tr>'
-		. '<td>' . _get_rank() . '</td>';
+		. '<td>' . _get_rank($_user['id']) . '</td>';
 	foreach ($heads as $head)
 	{
 		switch ($head[1])
@@ -215,6 +279,43 @@ foreach ($users as $_user)
 	</table>
 
 <div id="rank-list-navigator-bottom">
+
+<?php
+
+/**
+ * @ignore
+ */
+function _make_page_link($promt, $page)
+{
+	$url = t_get_link('show-ajax-rank-list', "$page", TRUE, TRUE);
+	echo "<a href=\"$url\" onclick=\"rank_list_naviage($page); return false;\">$promt</a>";
+}
+
+if ($start_page > 1)
+	_make_page_link('&lt;' . __('Prev'), $start_page - 1);
+if ($start_page < $total_page)
+{
+	if ($start_page > 1)
+		echo ' | ';
+	_make_page_link(__('Next') . '&gt;', $start_page + 1);
+}
+
+$id = _tf_get_random_id();
+$GoToPage = __('Go to page');
+echo <<<EOF
+<form action="#" id="rank-list-goto-form" method="post" onsubmit="rank_list_goto(); return false;">
+<label for="$id" style="float: left;">$GoToPage</label>
+<input value="$start_page" name="goto_page" id="$id" style="float: left; width: 30px;" type="text" />/$total_page
+</form>
+EOF;
+?>
+
+
+
+</div> <!-- id: rank-list-navigator-bottom -->
+
+</div> <!-- id: rank-list-content -->
+
 <script type="text/javascript">
 function rank_list_naviage(page)
 {
@@ -235,31 +336,12 @@ function rank_list_naviage(page)
 	});
 	return false;
 }
+function rank_list_goto()
+{
+	var page = $("#rank-list-goto-form input").val();
+	rank_list_naviage(page);
+}
+
 $(".rank-list-nickname-a").colorbox();
 table_set_double_bgcolor();
 </script>
-<?php
-$user_amount = $db->get_number_of_rows('users');
-$total_page = ceil($user_amount / $USERS_PER_PAGE);
-/**
- * @ignore
- */
-function _make_page_link($promt, $page)
-{
-	$url = t_get_link('show-ajax-rank-list', "$page", TRUE, TRUE);
-	echo "<a href=\"$url\" onclick=\"rank_list_naviage($page); return false;\">$promt</a>";
-}
-
-if ($start_page > 1)
-	_make_page_link('&lt;' . __('Prev'), $start_page - 1);
-if ($start_page < $total_page)
-{
-	if ($start_page > 1)
-		echo ' | ';
-	_make_page_link(__('Next') . '&gt;', $start_page + 1);
-}
-?>
-</div> <!-- id: rank-list-navigator-bottom -->
-
-</div> <!-- id: rank-list-content -->
-
