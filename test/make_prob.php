28817f9ad14e7345517c71f0c81dbf8c84a14faa
zxytim <zxytim@gmail.com>
2010-11-07 22:01:30 +0800

    ...

--- test/make_prob.php
+++ test/make_prob.php
@@ -49,6 +49,7 @@ function _make_prob($title)
 function make_prob()
 {
 	global $db, $DBOP;
+	/*
 	$db->insert_into('problems',
 		array('title' => 'A+B Problem',
 		'code' => 'a+b',
@@ -66,7 +67,7 @@ function make_prob()
 			'source' => 'Every OJ',
 			'hint' => '1 <= a, b <= 100000'
 		))
-	));
+	)); */
 	$db->insert_into('problems',
 		array('title' => 'A+B Problem, too',
 		'code' => 'a+b2',
