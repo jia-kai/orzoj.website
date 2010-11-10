813718b6a5281670e6fe54d5ed0a0e3b217c479b
zxytim <zxytim@gmail.com>
2010-11-10 18:39:29 +0800

    ...

--- contents/themes/default/faq.php
+++ contents/themes/default/faq.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * $File: faq.php
- * $Date: Wed Nov 10 15:59:29 2010 +0800
+ * $Date: Wed Nov 10 18:39:20 2010 +0800
  */
 /**
  * @package orzoj-website
@@ -38,8 +38,8 @@ function make_faq($Q, $A)
 
 $faq = array(
 	__('How do I know when to use a file as input and output and when not?') => __('Please take a careful look at the problem description, it will show you what to do.'),
-	__('Why should I share my code with others, for in many other Online Judges, they do not suppose to do so?') => __('We think the "Open Source" idea should spread to everyone here. Let you friends see your work will help them a lot, and you can get new thoughts via reading others code. Many great works is achieved by open source to people all over the world, and they helped improve it(e.g Linux). We hope everyone here share your code to others, and that will make a great learning environment.'),
-	__('I\'m not going to share my code, how can I close this function?') => __('We are so sorry to hear that, but we respect your dicision. You can determine users in some certain groups see your code, and you can just select `Nobody`.'),
+	__('Why should I share my code with others, for in many other Online Judges, they do not suppose to do so?') => __('We think the "Open Source" idea should spread to everyone here. Your code will give your friends some help, and you can get new thoughts via reading others code. Many great works are achieved by opening source to people all over the world, and they helped improve it(e.g Linux). We hope everyone here share your code to others, and that will make a great learning environment.'),
+	__('I\'m not going to share my code, how can I close this function?') => __('We are so sorry to hear that, but we respect your decision. You can determine users in some certain groups to see your code, and you can just select `Nobody`.'),
 	__('If I chose to share my code to everybody, what if they see my code during the contest?') => __('Don\'t worry, the visibility of code in the contest is determined by contest rules. In most type of the contest(like OI, ACM, etc.), the contest rule will forbid others from seeing your code. When the contest is finished, your code in the contest will be visible to others again.'),
 );
 $cnt = 0;
